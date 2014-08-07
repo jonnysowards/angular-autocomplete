b465e09b799d2b73578658224f062290a998a929
jonny <jtsowards@gmail.com>
2014-08-07 00:34:11 -0600

    Push

--- angular-autocomplete.js
+++ angular-autocomplete.js
@@ -1,74 +1,78 @@
 var app = angular.module('angularAutocomplete', []);
-app.directive('ngAutocomplete', function () {
+app.directive('ngAutocomplete', function ($http, $templateCache) {
     return {
         restrict: 'E',
-        require: '^acItems',
         scope: {
             acItems: '=acItems',
             acTerm: '=acTerm',
             acKeyup: '=acKeyup',
             ngModel: '=ngModel',
-            placeholder: '@placeholder',
-            acTemplate: '@acTemplate'
+            placeholder: '@placeholder'
         },
-        templateUrl: scope.acTemplate,
-        controller: ['$scope', function ($scope) {
-            $scope.acShow = false;
-            $scope.index = 0;
-            $scope.key = {left: 37, up: 38, right: 39, down: 40 , enter: 13, esc: 27, tab: 9};
-            $scope.updateAutoComplete = function (event) {
+	templateUrl: 'angular-autocomplete.html',
+    link: function (scope, $element, $attrs) {
+            scope.acShow = false;
+            scope.index = 0;
+	        scope.preSelect;
+            scope.key = {left: 37, up: 38, right: 39, down: 40 , enter: 13, esc: 27, tab: 9};
+            scope.updateAutoComplete = function (event) {
+                console.log(scope.index);
                 switch (event.keyCode) {
-                    case $scope.key.down:
-                        $scope.setAutocomplete();
-                        if($scope.acItems.length > $scope.index + 1){
-                            $scope.index ++;
+                    case scope.key.down:
+			//$scope.setAutocomplete();
+                        if(scope.acItems.length > scope.index + 1){
+                            scope.index ++;
                         }else{
-                            $scope.index = 0;
+                            scope.index = 0;
                         }
                         break;
-                    case $scope.key.up:
-                        if(-1 < $scope.index - 1){
-                            $scope.index --;
+                    case scope.key.up:
+                        if(0 <= scope.index - 1){
+                            scope.index --;
                         }else{
-                            $scope.index = $scope.acItems.length-1;
+                            scope.index = scope.acItems.length-1;
                         }
-                        $scope.setAutocomplete();
                         break;
-                    case $scope.key.enter:
-                    case $scope.key.right:
-                        $scope.acShow = false;
-                        $scope.setAutocomplete();
-                        $scope.acKeyup($scope.autocomplete);
-                        break;
-                    case $scope.key.esc:
-                    case $scope.key.tab:
-                        $scope.acShow = false;
+                    case scope.key.enter:
+                    case scope.key.right:
+			            scope.acShow = false;
+                        scope.setAutocomplete();
+			            console.log(scope.autocomplete);
+                        scope.acKeyup(scope.autocomplete);
+			break;
+                    case scope.key.esc:
+                    case scope.key.tab:
+			            scope.acShow = false;
                         break;
                     default:
-                        $scope.acShow = true;
-                        $scope.acKeyup($scope.autocomplete);
+                        scope.acShow = true;
+                        scope.acKeyup(scope.autocomplete);
                         break;
                 }
             }
 
-            $scope.checkForTab = function(event){
+            scope.checkForTab = function(event){
                 if(event.keyCode == 9){
-                    $scope.acShow = false;
-                    $scope.setAutocomplete();
-                    $scope.acKeyup($scope.autocomplete);
+                    scope.acShow = false;
+                    scope.setAutocomplete();
+                    scope.acKeyup(scope.autocomplete);
                 }
             }
-
-            $scope.setIndex = function(index){
-                $scope.index = index;
-                $scope.setAutocomplete();
-                $scope.acShow = false;
-                $scope.acKeyup($scope.autocomplete);
+			
+            scope.setIndex = function(index){
+                scope.index = index;
+                scope.setAutocomplete();
+		        scope.acShow = false;
+                scope.acKeyup(scope.autocomplete);
             }
 
-            $scope.setAutocomplete = function(){
-                $scope.autocomplete = angular.element('.autocompleteItems').find("li")[$scope.index].getText().replace(/\s/g, "");
-            }
-        }]
+
+            scope.setAutocomplete = function(){
+		        //console.log(angular.element('.autocompleteItems').find("li")[$scope.index].innerText);
+                console.log(scope.items);
+                scope.autocomplete = angular.element('.autocompleteItems').find("li")[scope.index].innerText.replace(/\s/g, "");
+	    	    scope.index = 0;
+	        }
+        }
     }
 });
--- example.js
+++ example.js
@@ -1,15 +1,41 @@
 var exampleApp = angular.module('exampleApp',
     [
-        "angularAutocomplete"
+	"ngTouch",
+	"angularAutocomplete"
     ]
-).controller('exampleController', function($scope) {
+).controller('exampleObjectController', function($scope) {
     $scope.items = [
         {partNum: "Q5950A"},
         {partNum: "Q5951A"},
         {partNum: "Q5952A"},
     ];
 
-    $scope.updateItem = function(typed){
-        console.log(typed);
+    console.log($scope.items[0]);
+
+    $scope.updateObjectItems = function(typedString){
+        console.log(typedString);
+        $scope.partToSelectObject = typedString;
     }
-});
+
+    
+    $scope.part = 0;
+    
+    $scope.test = "helloworld";
+}).controller('exampleStringController', function($scope) {
+        $scope.stringItems = [
+            "Q5950A",
+            "Q5951A",
+            "Q5952A"
+        ];
+
+
+        $scope.updateStringItems = function(typedObject){
+            console.log(typedObject);
+            $scope.partToSelectString = typedObject;
+        }
+
+
+        $scope.part = 0;
+
+        $scope.test = "helloworld";
+    });
